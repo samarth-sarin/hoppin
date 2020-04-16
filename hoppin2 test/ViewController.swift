1356f78c074616ce525d950b4576e7d0544d9480
samsarin <samsarin@umich.edu>
2020-04-16 13:14:29 -0400

    update with more features

--- hoppin2 test/ViewController.swift
+++ hoppin2 test/ViewController.swift
@@ -11,17 +11,47 @@ import MessageUI
 
 class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
     
+
+    
+    @IBOutlet weak var restaurantTextField: UITextField!
+    var selectedRestaurant: String?
+    var listOfRestaurant = ["Zingermans", "Frita Batidos", "Slurping Turtle", "Snap Pizza", "Tomokun", "Isalita", "Sava's", "Bufallo Wild Wings", "TK Wu", "Sadako", "Pizza House", "Charley's", "Brown Jug", "Taco Bell", "McDonalds", "Wendy's", "Subway", "Kangs", "Rich JC", "Belly Deli", "Tropical Smoothie", "Cold Stone"]
+    
     
     var num = "8108134200";
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.createAndSetupPickerView()
+        self.dismissAndClosePickerView()
     }
+    
+    func createAndSetupPickerView() {
+        let pickerview = UIPickerView()
+        pickerview.delegate = self
+        pickerview.dataSource = self
+        self.restaurantTextField.inputView = pickerview
+    }
+    
+    func dismissAndClosePickerView(){
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let button = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dismissAction))
+        toolBar.setItems([button], animated: true)
+        toolBar.isUserInteractionEnabled = true
+        self.restaurantTextField.inputAccessoryView = toolBar
+    }
+    
+    @objc func dismissAction() {
+        self.view.endEditing(true)
+    }
+    
     
     @IBAction func sendText(_ sender: UIButton) {
         if (MFMessageComposeViewController.canSendText()) {
             let controller = MFMessageComposeViewController()
-            controller.body = "This is our message"
+            controller.body = "My name is Samarth and I am ordering from Taco Bell"
             controller.recipients = [num]
             controller.messageComposeDelegate = self
             self.present(controller, animated: true, completion: nil)
@@ -41,5 +71,26 @@ class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
     override func viewWillDisappear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = false
     }
+    
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.listOfRestaurant.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.listOfRestaurant[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.selectedRestaurant = self.listOfRestaurant[row]
+        self.restaurantTextField.text = self.selectedRestaurant
+    }
+    
 }
 
